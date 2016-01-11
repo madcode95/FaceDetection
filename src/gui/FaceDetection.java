cbf7d97429a80493e4136a375f43f9734f3df056
Mantas <m.smaidrys@gmail.com>
2016-01-11 18:50:43 +0200

    1 - refactored exceptions to reduce boilerplate code, added error message output.

--- src/gui/FaceDetection.java
+++ src/gui/FaceDetection.java
@@ -63,7 +63,7 @@ public class FaceDetection extends javax.swing.JFrame {
                                 }
                             }
                         } catch (Exception ex) {
-                            System.out.println("Error");
+                            System.out.println(String.format("Error {0}", ex.getMessage()));
                         }
                     }
                 }
@@ -179,13 +179,10 @@ public class FaceDetection extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | 
+                 InstantiationException | 
+                 IllegalAccessException | 
+                 javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(FaceDetection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
